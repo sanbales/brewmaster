062672d50f436424a06e0210a27a90a2f6c80d8d
Balestrini, Santiago <Santiago.Balestrini@gtri.gatech.edu>
2015-09-19 15:32:13 -0400

    added setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from pip.req import parse_requirements
+from pip.download import PipSession
+from setuptools import setup, find_packages
+
+
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+requirements = [str(ir.req) for ir in install_reqs]
+
+setup(name='brewmaster',
+      version='0.0.1',
+      description='A model-based framework to assist breweries in optimizing their processes.',
+      long_description=open('README.md').read(),
+      download_url='https://github.com/sanbales/brewmaster',
+      keywords='brewing beer process optimization',
+      author='Santiago Balestrini-Robinson',
+      author_email='sanbales@gmail.com',
+      url='https://github.com/sanbales/brewmaster',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=requirements,
+      classifiers=['Development Status :: 2 - Pre-Alpha',
+                   'License :: OSI Approved :: MIT License',
+                   'Natural Language :: English',
+                   'Framework :: IPython',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: POSIX :: Linux',
+                   'Operating System :: Microsoft :: Windows',
+                   'Programming Language :: Python :: 2.7',
+                   ],
+      py_modules=['brewmaster'])
